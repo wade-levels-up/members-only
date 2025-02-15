760349f47fe48dcf72773746f15f41e17fb54fb5
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-02-16 08:31:05 +1100

    Make head and footer ejs partials, fix msg delete

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 
 const getHomepage = asyncHandler(async (req, res) => {
   try {
-    res.render("pages/index", { title: "Home" });
+    res.render("pages/index", { title: "Home", user: req.user });
   } catch (error) {
     throw new Error(`Couldn't get the home page: ${error}`);
   }
--- middleware/ensureAuthenticated.js
+++ middleware/ensureAuthenticated.js
@@ -2,21 +2,21 @@ function isAuth(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.render("pages/unauthorized"); // Route for not authenticated
+  res.render("pages/unauthorized", { title: "Unauthorized" }); // Route for not authenticated
 }
 
 function isMember(req, res, next) {
   if (req.isAuthenticated() && req.user.is_member) {
     return next();
   }
-  res.render("pages/unauthorized"); // Route for not authenticated
+  res.render("pages/unauthorized", { title: "Unauthorized" }); // Route for not authenticated
 }
 
 function isAdmin(req, res, next) {
   if (req.isAuthenticated() && req.user.is_admin) {
     return next();
   }
-  res.render("pages/unauthorized"); // Route for not authenticated
+  res.render("pages/unauthorized", { title: "Unauthorized" }); // Route for not authenticated
 }
 
 module.exports = { isAuth, isMember, isAdmin };
